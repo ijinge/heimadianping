6ff88a9c5edaa446cc5607b9cba9f2c7c20060dc
ijinge <925322357@qq.com>
2025-01-11 15:21:49 +0800

    添加了一个ReFreshTokenInterceptor用于刷新用户信息缓存的时间

---
+++ src/main/java/com/hmdp/utils/ReFreshTokenInterceptor.java
@@ -0,0 +1,46 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.hmdp.dto.UserDTO;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.LOGIN_USER_KEY;
+import static com.hmdp.utils.RedisConstants.LOGIN_USER_TTL;
+
+public class ReFreshToken implements HandlerInterceptor {
+
+    private final RedisTemplate<String,Object> redisTemplate;
+
+    public ReFreshToken(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1、获取token，并判断token是否存在
+        String token = request.getHeader("authorization");
+        // 2.未登录则放行交给登录拦截器去处理
+        if(token==null){
+            return  true;
+        }
+        // 查询redis中是否有用户信息
+        Map<Object, Object> userMap = redisTemplate.opsForHash().entries(LOGIN_USER_KEY + token);
+        // 无用户信息 则放行登录
+        if(userMap.isEmpty()){
+            return true;
+        }
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+        // 存在说明已经登录 放入ThreadLocal
+        UserHolder.saveUser(userDTO);
+        // 重置用户信息过期时间
+        redisTemplate.expire(LOGIN_USER_KEY + token,LOGIN_USER_TTL, TimeUnit.MINUTES);
+        return true;
+    }
+
+}
